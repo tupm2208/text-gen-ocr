2b7fdeb67be35fcb9377afd65bf75ca110cd6dc7
tupm <tu.phamminh.2207@gmail.com>
2019-07-02 14:08:09 +0700

    add run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+python ./run.py -l pass \
+                -e png \
+                -b 1 \
+                -na 2 \
+                -f 50 \
+                -c 40 \
+                -al 0 \
+                -t 3
