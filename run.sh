070233d4761c8474651701e94a20eb69710ce250
tupm <tu.phamminh.2207@gmail.com>
2019-07-02 15:48:13 +0700

    change params in run file

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+rm -rf out/*
+rm -rf val/*
+rm -rf test/*
 python ./run.py -l pass \
                 -e png \
                 -b 1 \
                 -na 2 \
                 -f 50 \
-                -c 40 \
+                -c 60 \
                 -al 0 \
                 -t 3
+rm out/labels.txt
