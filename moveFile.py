a5f50b4adce84cf65cd0bd4e0943581313274d53
tupm <tu.phamminh.2207@gmail.com>
2019-07-08 11:10:09 +0700

    re split

--- moveFile.py
+++ moveFile.py
@@ -2,17 +2,13 @@ import os
 import random
 
 folders = [os.path.join('out', folder) for folder in os.listdir('out')]
-os.makedirs('test', exist_ok=True)
+os.makedirs('val', exist_ok=True)
 
 for e in folders:
     if e == 'out/labels.txt':
         continue
-    os.makedirs(e.replace('out', 'test'), exist_ok=True)
+    os.makedirs(e.replace('out', 'val'), exist_ok=True)
     images = [os.path.join(e, image) for image in os.listdir(e)]
     random.shuffle(images)
-    for i in range(0, int(len(images)/5)):
-        os.rename(images[i], images[i].replace('out', 'test'))
-
-    os.makedirs(e.replace('out', 'val'), exist_ok=True)
-    for i in range(int(len(images)/5), int(len(images)/5) + int(len(images)/25)):
+    for i in range(0, int(len(images)/10)):
         os.rename(images[i], images[i].replace('out', 'val'))
--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ python ./run.py -l pass \
                 -b 1 \
                 -na 2 \
                 -f 50 \
-                -c 2 \
+                -c 4 \
                 -al 0 \
                 -t 10 \
                 -d 3
